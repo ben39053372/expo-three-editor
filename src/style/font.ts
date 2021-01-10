f8817997a7bc2b6b1ed65bfd5932a8b71aac8d76
ben39053372 <ben39053372@gmail.com>
2021-01-10 13:55:29 +0800

    add style sheet

---
+++ src/style/font.ts
@@ -0,0 +1,21 @@
+import { StyleSheet } from "react-native"
+
+const font = StyleSheet.create({
+  h1: {
+    fontSize: 20
+  },
+  h2: {
+    fontSize: 18
+  },
+  h3: {
+    fontSize: 16
+  },
+  h4: {
+    fontSize: 14
+  },
+  h5: {
+    fontSize: 12
+  }
+})
+
+export default font
