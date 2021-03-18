34956074055ce016d8e9193aa2589a203a3b159b
Ben Cheng <bencheng@skylight-group.com>
2021-03-18 09:47:58 +0800

    add mouse wheel event

--- src/Canvas/index.tsx
+++ src/Canvas/index.tsx
@@ -8,6 +8,7 @@ import { ExpoWebGLRenderingContext, GLView } from "expo-gl"
 import MyScene from "@Editor/MyScene"
 import FPSStats from "react-fps-stats"
 import { Platform } from "react-native"
+import useMouseWheel from "./Hooks/useMouseWheel"
 
 export const webGLInstance = new WebGL()
 
@@ -29,17 +30,18 @@ const Canvas = (props: CanvasProps) => {
 
   useWindowResize(webGL)
   useKeyboard(webGL)
+  useMouseWheel(webGL)
 
   return (
     <UIView webGL={webGL}>
       <GestureView webGL={webGL}>
-        {/* {Platform.OS === "web" && <FPSStats top={50} />}
+        {Platform.OS === "web" && <FPSStats top={50} />}
         <GLView
           style={{ flex: 1 }}
           onContextCreate={(gl: ExpoWebGLRenderingContext) => {
             webGL.onGLContextCreate(gl, scene)
           }}
-        /> */}
+        />
       </GestureView>
     </UIView>
   )
