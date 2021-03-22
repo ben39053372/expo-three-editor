59297a57d0633bad1b7d6951de1ea3d18079a767
Ben Cheng <bencheng@skylight-group.com>
2021-03-22 09:56:02 +0800

    change camera to use interface from abstract class

--- src/Canvas/Camera.ts
+++ src/Canvas/Camera.ts
@@ -1,27 +1,21 @@
 import { THREE } from "expo-three"
-
-abstract class Camera extends THREE.Camera {
-  abstract movementSpeed: number
-  abstract rotateSpeed: number
-  abstract useForPanObj: THREE.Object3D
-
-  abstract raycaster: THREE.Raycaster
-
-  abstract init(): void
-
-  abstract move(axis2D: THREE.Vector2, speed?: number): void
-
-  abstract rotateByAxis2D(axis2D: THREE.Vector2): void
-
-  abstract panCamera(axis2D: THREE.Vector2, speed?: number): void
-
-  abstract shootRaycaster(coords: { x: number; y: number }): void
-
-  abstract getIntersectObject(object: THREE.Object3D): THREE.Intersection[]
-
-  abstract getIntersectObjects(objects: THREE.Object3D[]): THREE.Intersection[]
-
-  abstract updateOnResize(width: number, height: number): void
+import WebGl from "./WebGL"
+interface Camera extends THREE.Camera {
+  movementSpeed: number
+  rotateSpeed: number
+  useForPanObj: THREE.Object3D
+  lookAtRaycaster: THREE.Raycaster
+  cameraLookAt: THREE.Vector3
+  raycaster: THREE.Raycaster
+  init(): void
+  move(axis2D: THREE.Vector2, speed?: number): void
+  rotateByAxis2D(axis2D: THREE.Vector2): void
+  panCamera(axis2D: THREE.Vector2, speed?: number): void
+  shootRaycaster(coords: { x: number; y: number }): void
+  getIntersectObject(object: THREE.Object3D): THREE.Intersection[]
+  getIntersectObjects(objects: THREE.Object3D[]): THREE.Intersection[]
+  updateOnResize(width: number, height: number): void
+  updateLookAt(webGL: WebGl): void
 }
 
 export default Camera
